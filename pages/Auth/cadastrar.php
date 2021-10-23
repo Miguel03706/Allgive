9e133b60f9683a002a8f4857e24fff09b0e10cee
QuartetoeMascotes <migueltjsilva@gmail.com>
2021-10-23 20:35:32 -0300

    cadastrar ongs adicionado visualmente

--- pages/Auth/cadastrar.php
+++ pages/Auth/cadastrar.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar</title>
-    
+
 </head>
 
 <body>
@@ -20,12 +20,12 @@
     <div class="main">
         <div class="container-fluid header">
             <div class="row align-items-center topo">
-                <aside class="col-md-6 text-center" onClick="exibir()">Doador</aside>
-                <aside class="col-md-6 text-center" onClick="ocultar()">ONG</aside>
+                <aside class="col-md-6 text-center" onClick="exibir()" id="doador">Doador</aside>
+                <aside class="col-md-6 text-center" onClick="ocultar()" id="ong">ONG</aside>
             </div>
         </div>
 
-        <div class="form-donate" name="form_donate"> 
+        <div class="form-donate" id="form_donate">
             <form action="" method="post">
                 <p>
                     <label for="email">Email: </label>
@@ -41,6 +41,60 @@
             </form>
         </div>
 
+        <div class="form-ong" id="form_ong">
+            <form action="" method="post">
+                <p>
+                    <label for="email">Email: </label>
+                    <input type="text" name="email" placeholder="Digite seu Email" class="form-control" required>
+                </p>
+                <p>
+                    <label for="password">Senha: </label>
+                    <input type="password" name="password" class="form-control" placeholder="Digite sua senha" required>
+                </p>
+                <p>
+                    <label for="organizacao">Nome da organização: </label>
+                    <input type="text" name="organizacao" class="form-control" placeholder="Digite o nome de sua ONG" required>
+
+                </p>
+                <p>
+                <div class="container-fluid">
+                    <div class="row">
+                        <aside class="col-md-4">
+                            <label for="membros">Quantidade de membros: </label>
+                            <input type="number" name="membros" class="form-control" placeholder="Quantidade de membros" min="5" required>
+                        </aside>
+                        <aside class="col-md-4">
+                            <label for="estado">Estado: </label>
+                            <input type="select" name="estado" class="form-control" placeholder="" required>
+                        </aside>
+                        <aside class="col-md-4">
+                            <label for="cidade">Cidade: </label>
+                            <input type="select" name="cidade" class="form-control" placeholder="" required>
+                        </aside>
+
+                    </div>
+                </div>
+                </p>
+                <p>
+                    <label for="cep">CEP: </label>
+                    <input type="number" name="cep" class="form-control" placeholder="Digite seu CEP" required>
+
+                </p>
+                <p>
+                    <label for="cnpj">CNPJ: </label>
+                    <input type="number" name="cnpj" class="form-control" placeholder="Digite seu CNPJ" required>
+                </p>
+                <p>
+                    <label for="cpf">CPF: </label>
+                    <input type="number" name="cnpj" class="form-control" placeholder="Digite seu CPF" required>
+                </p>
+                <p class="col-md-12 text-center">
+                    <input type="submit" value="Enviar" name="submit" class="btn-group-sm">
+                </p>
+            </form>
+        </div>
+
+
     </div>
 
 </body>
@@ -48,12 +102,12 @@
 </html>
 
 <?php
-    if(isset($_POST["submit"])){
-        $email = $_POST["email"];
-        $id = sha1(uniqid(rand(), true));
-        $senha = sha1($_POST["password"]);
-        
+if (isset($_POST["submit"])) {
+    $email = $_POST["email"];
+    $id = sha1(uniqid(rand(), true));
+    $senha = sha1($_POST["password"]);
 
 
-       // echo "<div class='col-md-6'>email: $email | senha: $senha| id: $id')</div>";
-    };
+
+    // echo "<div class='col-md-6'>email: $email | senha: $senha| id: $id')</div>";
+};
--- pages/Auth/cadastro.js
+++ pages/Auth/cadastro.js
@@ -1,9 +1,22 @@
 function exibir() {
     let div = document.getElementById("form_donate");
-    div.style.color = 'red';
+    let doador = document.getElementById("doador");
+    let ong = document.getElementById("ong");
+
     div.style.display = "block";
+    doador.style.backgroundColor = "#c52ea3";
+    ong.style.backgroundColor = "#dc81c8";
+    document.getElementById("form_ong").style.display = "none";
+
 }
 
 function ocultar() {
+    let doador = document.getElementById("doador");
+    let ong = document.getElementById("ong");
+
+    doador.style.backgroundColor = "#dc81c8";
+    ong.style.backgroundColor = "#c52ea3";
     document.getElementById("form_donate").style.display = "none";
+    document.getElementById("form_ong").style.display = "block";
+
 }
